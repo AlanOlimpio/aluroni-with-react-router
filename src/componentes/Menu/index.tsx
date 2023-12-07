dfb55881d940ec7c02bcd12c4d350dafab102b40
Alan Olimpio <alancleyton.olimpio@gmail.com>
2023-12-07 08:16:59 -0300

    fix: corrige import logo

--- src/componentes/Menu/index.tsx
+++ src/componentes/Menu/index.tsx
@@ -1,5 +1,5 @@
 import styles from './Menu.module.scss';
-import { ReactComponent as Logo } from 'assets/logo.svg';
+import { ReactComponent as Logo } from '../../assets/logo.svg';
 import { Link } from 'react-router-dom';
 
 
